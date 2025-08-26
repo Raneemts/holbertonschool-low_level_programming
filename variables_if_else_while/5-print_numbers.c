c3ecfa1d6074154fba21804c7b630b1c946c9f8d
raneemts <raneem_alsaqat@hotmail.com>
2025-08-26 17:05:27 +0000

    numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,9 +2,6 @@
 /**
  * main - Entry point
  *
- * prints all single digit numbers of base 10 starting from 0, followed by a new line.
- * followed by a new line using putchar.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
