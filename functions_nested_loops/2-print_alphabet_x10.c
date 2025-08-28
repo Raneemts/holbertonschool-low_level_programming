b2f09e6bd439f4745504febb5e66ccb8b19ce058
raneemts <raneem_alsaqat@hotmail.com>
2025-08-28 20:21:30 +0000

    alpahbet 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include"main.h"
 /**
- *
- * main
- * description:print the alphabet 10 times
- * return:nothing
+ * print_alphabet_x10 - Check main
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: Nothing.
  */
 void print_alphabet_x10(void)
 {
