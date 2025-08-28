a74bec560fa27a3c8430aa7caf9e2344cf69041a
raneemts <raneem_alsaqat@hotmail.com>
2025-08-28 19:47:54 +0000

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"headerfile.h"
+/**
+ * main - Check description
+ * Description: It prints the word putchar, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	char str[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+	return (0);
+}
