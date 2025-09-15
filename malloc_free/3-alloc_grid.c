64603bcb4f0b81e1ab3cdb42abe7d9c18a50d664
raneemts <raneem_alsaqat@hotmail.com>
2025-09-15 11:30:45 +0000

    2d array

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,17 +9,15 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **array, i, j;
+	int **array, i = 0, j = 0;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	array = (int **)malloc(sizeof(int *) * height);
-
 	if (array == NULL)
 		return (NULL);
-
-	for (i = 0; i < height; i++)
+	for (; i < height; i++)
 	{
 		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
@@ -27,7 +25,7 @@ int **alloc_grid(int width, int height)
 	}
 
 	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
+		for (; j < width; j++)
 			array[i][j] = 0;
 	return (array);
 }
