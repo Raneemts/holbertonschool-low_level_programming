09fb3a87b14697efe25c34a03beae8d8e8ad88e9
raneemts <raneem_alsaqat@hotmail.com>
2025-09-21 15:52:50 +0000

    ss

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,2 +1,11 @@
-int _putchar(char c);
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+ * print_name - prints a name using a callback
+ * @name: string to print
+ * @f: function pointer
+ */
 void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
