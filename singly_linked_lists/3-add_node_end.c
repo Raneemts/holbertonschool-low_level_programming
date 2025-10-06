852e1cad5e719581f5aae08e15a87a4b5c8ff35e
raneemts <raneem_alsaqat@hotmail.com>
2025-10-06 14:52:31 +0000

    dd

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end - adds a node at the end of a linked list
+ * @head: pointer to a pointer to the head node
+ * @str: string to add as node content
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+	unsigned int length = 0;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	while (str[length])
+		length++;
+
+	new_node->len = length;
+	new_node->str = strdup(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+
+	return (new_node);
+}
