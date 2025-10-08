642b3415725cd30d40c4cf00c51e0ad146b30507
raneemts <raneem_alsaqat@hotmail.com>
2025-10-08 15:28:26 +0000

    create file

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, ssize_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
